efea6c188c688a0e993c3a1bc8ad032ece1c2e3f
Duncan Ritchie <duncanritchie@btinternet.com>
2019-08-29 11:14:58 +0100

    Added some more questions

--- main.js
+++ main.js
@@ -44,6 +44,111 @@ const categories = [{
                     correct: false
                 }
             ]
+        },
+        {
+            q: "Burundi, DR Congo, Tanzania, and Zambia have shoreline on which lake?",
+            answers: [
+                {
+                    a: "Lake Turkana",
+                    correct: false
+                },
+                {
+                    a: "Lake Victoria",
+                    correct: false
+                },
+                {
+                    a: "Lake Tanganyika",
+                    correct: true
+                },
+                {
+                    a: "Lake Malawi",
+                    correct: false
+                }
+            ]
+        },
+        {
+            q: "What group of small islands is immediately south of St Vincent in the Caribbean?",
+            answers: [
+                {
+                    a: "Bahamas",
+                    correct: false
+                },
+                {
+                    a: "Netherlands Antilles",
+                    correct: false
+                },
+                {
+                    a: "Hawaii",
+                    correct: false
+                },
+                {
+                    a: "Grenadines",
+                    correct: true
+                }
+            ]
+        },
+        {
+            q: "So far this year, Brazil's National Institute for Space Research has recorded (roughly) how many wildfires in the country?",
+            answers: [
+                {
+                    a: "40 in the Amazon, 3,000 elsewhere",
+                    correct: false
+                },
+                {
+                    a: "400 in the Amazon, 1,200 elsewhere",
+                    correct: false
+                },
+                {
+                    a: "4,000 in the Amazon, 15,000 elsewhere",
+                    correct: false
+                },
+                {
+                    a: "40,000 in the Amazon, 35,000 elsewhere",
+                    correct: true
+                }
+            ]
+        },
+        {
+            q: "What is the name of the mountain range that runs roughly north-south along most of mainland Italy?",
+            answers: [
+                {
+                    a: "Golan Heights",
+                    correct: false
+                },
+                {
+                    a: "Ammonites",
+                    correct: false
+                },
+                {
+                    a: "Dolomites",
+                    correct: false
+                },
+                {
+                    a: "Apennines",
+                    correct: true
+                }
+            ]
+        },
+        {
+            q: "What is the deepest lake in the world?",
+            answers: [
+                {
+                    a: "Lake Baikal",
+                    correct: true
+                },
+                {
+                    a: "Lake Maracaibo",
+                    correct: false
+                },
+                {
+                    a: "Loch Ness",
+                    correct: false
+                },
+                {
+                    a: "Lake Superior",
+                    correct: false
+                }
+            ]
         }
     ]
 },
@@ -63,6 +168,27 @@ const categories = [{
                 },
                 {
                     a: "Is it a monstaaaa?",
+                    correct: true
+                }
+            ]
+        },
+        {
+            q: "'Clair de Lune' is a famous piece composed around 1890 by which French composer?",
+            answers: [
+                {
+                    a: "Hector Berlioz",
+                    correct: false
+                },
+                {
+                    a: "Georges Bizet",
+                    correct: false
+                },
+                {
+                    a: "Frédéric Chopin",
+                    correct: false
+                },
+                {
+                    a: "Claude Debussy",
                     correct: true
                 }
             ]
@@ -131,6 +257,27 @@ const categories = [{
             ]
         },
         {
+            q: "Who won the Eurovision Song Contest for the Netherlands in 2019?",
+            answers: [
+                {
+                    a: "Duncan Laurence",
+                    correct: true
+                },
+                {
+                    a: "Duncan Sheik",
+                    correct: false
+                },
+                {
+                    a: "Gary Duncan",
+                    correct: false
+                },
+                {
+                    a: "Lincoln Duncan",
+                    correct: false
+                }
+            ]
+        },
+        {
             q: "What was number one in the UK singles chart when I was born?",
             answers: [
                 {
@@ -166,10 +313,11 @@ openCategory = (i) => {
 
 openQuestion = (catNum, qNum) => {
     console.log("The first question of category "+catNum+" would load now.");
-    document.getElementById("question-head").textContent = "Question "+qNum;
+    document.getElementById("question-head").textContent = "Question "+(qNum+1);
     document.getElementById("question").textContent = categories[catNum].questions[qNum].q;
     
     let answers = categories[catNum].questions[qNum].answers;
+    let answerList = document.getElementById("answer-list")
     answers.map((a, i)=>{
         let node = document.createTextNode(a.a);
         let button = document.createElement("button");
@@ -179,7 +327,7 @@ openQuestion = (catNum, qNum) => {
     
         let li = document.createElement("li");
         li.appendChild(button);
-        document.getElementById("answer-list").appendChild(li);
+        answerList.appendChild(li);
     })
 }
 
